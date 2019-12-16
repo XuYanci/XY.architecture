a81002540fb761c6991d81388e5029fae58e7843
yanci <grandy.wind@gmail.com>
2019-12-16 23:10:59 +0800

    1. add head first source code 2. exercise simple factory

--- DesignPattern/DesignPattern/main.m
+++ DesignPattern/DesignPattern/main.m
@@ -8,10 +8,74 @@
 
 #import <Foundation/Foundation.h>
 
+/// Simple Factory
+
+@interface Door : NSObject
+
+- (float)getWidth;
+- (float)getHeight;
+
+@end
+
+@implementation Door
+
+- (float)getWidth {return 0;}
+- (float)getHeight {return 0;}
+
+@end
+
+@interface WoodenDoor : Door
+
+@property (nonatomic,assign) float width;
+@property (nonatomic,assign) float height;
+
+@end
+
+@implementation WoodenDoor
+
+- (id)initWithWidth:(float) width height:(float)height {
+    if (self = [super init]) {
+        _width = width;
+        _height = height;
+    }
+    return self;
+}
+
+- (float)getWidth {
+    return _width;
+    
+}
+
+- (float)getHeight {
+    return _height;
+}
+
+@end
+
+@interface DoorFactory : NSObject {
+    
+}
++ (Door *) makeDoor:(float)width height:(float)height;
+@end
+@implementation DoorFactory
+
++ (Door *) makeDoor:(float)width height:(float)height {
+    return [[WoodenDoor alloc]initWithWidth:10 height:10 ];
+}
+
+
+@end
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, Design Pattern!");
     }
+    
+    /// Simple Factory
+    Door *door =  [DoorFactory makeDoor:20 height:20];
+    [door getWidth];
+    [door getHeight];
+    
     return 0;
 }
